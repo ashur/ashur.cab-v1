71a00e524259583451490bd4c2d2c763fac28d98
Ashur Cabrera <hello@cabreramade.co>
2017-12-31 15:34:03 -0800

    🥤 Adding articles:build

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,16 @@
 const gulp = require( 'gulp' );
 
+var paths =
+{
+	articles: {
+		src: "src/articles/published",
+		dest: "dist/articles"
+	}
+};
+
+gulp.task( 'articles:build', function()
+{
+	return gulp
+		.src( `${paths.articles.src}/**/*.md` )
+		.pipe( gulp.dest( `${paths.articles.dest}` ) );
+});
